8763ac107252606221487bd4596ae06f358f7886
waxandwing <kellynyhagen@gmail.com>
2026-08-31 22:51:12 -0400

    Load range interaction styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./arc-interactions.css";
 import "./range-views.css";
+import "./range-interactions.css";
 import "./arc-visual-language.css";
 import "./onboarding-screen.css";
 import "./week-planner.css";
