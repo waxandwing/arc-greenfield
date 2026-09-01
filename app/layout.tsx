2580081f7e1ea4a86b94b1b7edccd2410b551303
waxandwing <kellynyhagen@gmail.com>
2026-09-01 16:05:22 -0400

    Use one Arc workspace style owner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "./arc-interactions.css";
-import "./range-views.css";
-import "./range-interactions.css";
-import "./arc-visual-language.css";
 import "./onboarding-screen.css";
-import "./week-planner.css";
 import "./workspace-rebuild.css";
 
 export const metadata: Metadata = {
