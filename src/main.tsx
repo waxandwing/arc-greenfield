736c5666e25c44464de99232a9f7b6627faeddda
waxandwing <kellynyhagen@gmail.com>
2026-09-02 21:24:44 -0400

    Load calendar control styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/tokens.css'
 import './styles/global.css'
+import './styles/calendarControls.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>,
