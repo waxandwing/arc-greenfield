8f810ba1a2d5eab7d01f8a40b4392c2207b35e55
waxandwing <kellynyhagen@gmail.com>
2026-09-02 21:52:56 -0400

    Load calendar readability audit styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles/tokens.css'
 import './styles/global.css'
 import './styles/calendarControls.css'
+import './styles/calendarReadability.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>,
