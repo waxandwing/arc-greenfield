b001d72b3da71602448ed9afacd83f92c866325d
waxandwing <kellynyhagen@gmail.com>
2026-09-02 20:27:53 -0400

    Define canonical calendar navigation views

---
+++ src/navigation/calendarViews.ts
@@ -0,0 +1,5 @@
+export const CALENDAR_VIEWS = ['Year', 'Quarter', 'Month', 'Week', 'Day'] as const
+
+export type CalendarView = (typeof CALENDAR_VIEWS)[number]
+
+export const DEFAULT_HOME_VIEW: CalendarView = 'Month'
