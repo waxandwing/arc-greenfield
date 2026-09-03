daa431ef19a839e415363502ed5de0d0f5c8023c
waxandwing <kellynyhagen@gmail.com>
2026-09-02 22:11:44 -0400

    Load term setup styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles/tokens.css'
 import './styles/global.css'
 import './styles/calendarControls.css'
 import './styles/calendarReadability.css'
+import './styles/termSetup.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>,
