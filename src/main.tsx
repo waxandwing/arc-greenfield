f79a56ec91a411eab5c005b57e5e5b84d827fdf2
waxandwing <kellynyhagen@gmail.com>
2026-09-07 18:05:51 -0400

    Preserve local planner regression entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 import { DeploymentRouter } from './deployment/DeploymentRouter'
 import './styles/tokens.css'
 import './styles/global.css'
@@ -16,6 +17,9 @@ import './styles/recoveryReview.css'
 import './styles/sourceCalendarReview.css'
 import './styles/schoolIdentitySearch.css'
 
+const localPlannerRegression = ['localhost', '127.0.0.1', '0.0.0.0'].includes(window.location.hostname)
+  && window.location.pathname === '/'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><DeploymentRouter /></React.StrictMode>,
+  <React.StrictMode>{localPlannerRegression ? <App /> : <DeploymentRouter />}</React.StrictMode>,
 )
