2e6c5a08ef45ed143bc2f02ac26929d71fc86c00
waxandwing <kellynyhagen@gmail.com>
2026-09-16 13:38:37 -0400

    Expose gold master fixture gallery route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import { AppFrame } from './components/AppFrame'
 import { DeskBuildStamp } from './components/DeskBuildStamp'
+import { GoldMasterGallery } from './components/GoldMasterGallery'
 import { IcarusEntryFlow } from './entry/IcarusEntryFlow'
 import { isEntryComplete } from './entry/entryAccess'
 
+function goldMasterRequested() {
+  const params = new URLSearchParams(window.location.search)
+  return params.has('gold') || params.get('gallery') === 'gold-master'
+}
+
 export default function App() {
   const [entryComplete, setEntryComplete] = useState(() => isEntryComplete())
+
+  if (goldMasterRequested()) {
+    return <GoldMasterGallery />
+  }
 
   if (!entryComplete) {
     return <IcarusEntryFlow onComplete={() => setEntryComplete(true)} />
